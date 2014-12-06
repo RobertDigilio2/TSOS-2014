67b6af233c71607f5701020500c09aaf2c5a65a5
RobertDigilio2 <elitemajor02@hotmail.com>
2014-12-06 17:29:36 -0500

    1.0 Update
    Added scrolling, status bar, character code validation, date/time and status on canvas, bsod, and some minor fixes.

--- source/scripts/globals.ts
+++ source/scripts/globals.ts
@@ -13,8 +13,17 @@
 // TODO: Make a global object and use that instead of the "_" naming convention in the global namespace.
 //
 var APP_NAME: string    = "ROBOS";   // 'cause Bob and I were at a loss for a better name.
-var APP_VERSION: string = "0.50";   // What did you expect?
+var APP_VERSION: string = "1.0";   // What did you expect?
+
+//The Status
 var STATUS: string = "Type command: status <string> to change your status";
+
+//Status Bar variables
+var _BarCanvas: HTMLCanvasElement = null;
+var _BarHandler = null;
+var _BarContext = null;
+
++var _ProgramInput = null;
 
 var CPU_CLOCK_INTERVAL: number = 100;   // This is in ms, or milliseconds, so 1000 = 1 second.
 
