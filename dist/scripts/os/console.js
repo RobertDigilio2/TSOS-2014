acbb61788b9658bd3b99da07668a06cbc3cf9ea0
RobertDigilio2 <elitemajor02@hotmail.com>
2014-12-06 17:29:48 -0500

    Update 1.0
    Added scrolling, status bar, character code validation, date/time and status on canvas, bsod, and some minor fixes.

--- dist/scripts/os/console.js
+++ dist/scripts/os/console.js
@@ -58,25 +58,27 @@ var TSOS;
                 } else {
                     if (chr === String.fromCharCode(8)) {
                         var clearChar = this.buffer.charAt(this.buffer.length - 1);
-                        this.buffer = this.buffer.substring(0, this.buffer.length - 1);
                         this.backSpace(clearChar);
+                        this.buffer = this.buffer.substring(0, this.buffer.length - 1);
 
                     } else {
                         //Tab
                         if (chr == String.fromCharCode(9)) {
                             var currentBuffer = this.buffer.toString();
++                            var returnBuffer = "";
                             var foundMatch = false;
-                            var currentCommands = ["ver", "help", "shutdown", "cls", "man", "trace", "rot13", "prompt", "status", "datetime"];
+                            var currentCommands = ["ver", "help", "shutdown", "cls", "man", "trace", "rot13", "prompt", "load", "bsod", "status", "datetime"];
  
                             for (var k = 0; k < currentCommands.length; k++) {
-                                if ((this.contains(currentBuffer, currentCommands[k])) && foundMatch == false) {
-                                    currentBuffer = currentCommands[k];
++                                if ((this.inOrderContains(currentBuffer, currentCommands[k]))) {
++                                    returnBuffer += currentCommands[k];
++                                    returnBuffer += " ";
                                     foundMatch = true;
                                 }
                             }
 
                             if (foundMatch) {
-                                this.replaceBuffer(currentBuffer);
+                                this.replaceBuffer(returnBuffer);
                             }
                         } else {
                             if (chr == "up") {
@@ -95,10 +97,6 @@ var TSOS;
                                 } else {
                                     // This is a "normal" character, so ...
                                     // ... draw it on the screen...
-                                    //the first wrapping text attempt
-                                    // if ((this.buffer.length % 47) == 0 && this.buffer.length != 0) {
-                                    //   this.advanceLine();
-                                    //}
                                     this.putText(chr);
 
                                     // ... and add it to our buffer.
@@ -119,36 +117,68 @@ var TSOS;
             // do the same thing, thereby encouraging confusion and decreasing readability, I
             // decided to write one function and use the term "text" to connote string or char.
             // UPDATE: Even though we are now working in TypeScript, char and string remain undistinguished.
-            if (text !== "") {
-                // Draw the text at the current X and Y coordinates.
-                _DrawingContext.drawText(this.currentFont, this.currentFontSize, this.currentXPosition, this.currentYPosition, text);
-
-                // Move the current X position.
-                var offset = _DrawingContext.measureText(this.currentFont, this.currentFontSize, text);
-                this.currentXPosition = this.currentXPosition + offset;
+            
+            //Line wrapping
+            if (text.length > 1) {
++                for (var i = 0; i < text.length; i++) {
++                    this.putText(text.charAt(i));
++                }
++            } else {
++                if (text !== "") {
++                    // Draw the text at the current X and Y coordinates.
++                    var offset = _DrawingContext.measureText(this.currentFont, this.currentFontSize, text);
++                    if ((this.currentXPosition + offset) > _DrawingContext.canvas.width) {
++                        this.advanceLine();
++                    }
++                    _DrawingContext.drawText(this.currentFont, this.currentFontSize, this.currentXPosition, this.currentYPosition, text);
+ 
++                    // Move the current X position.
++                    this.currentXPosition = this.currentXPosition + offset;
++                }
             }
         };
 
         Console.prototype.advanceLine = function () {
             this.currentXPosition = 0;
-
-            /*
-            * Font size measures from the baseline to the highest point in the font.
-            * Font descent measures from the baseline to the lowest point in the font.
-            * Font height margin is extra spacing between the lines.
-            */
-            this.currentYPosition += _DefaultFontSize + _DrawingContext.fontDescent(this.currentFont, this.currentFontSize) + _FontHeightMargin;
++            //Scrolling
++            if ((this.currentYPosition + _DefaultFontSize + _FontHeightMargin) < _DrawingContext.canvas.height) {
++                this.currentYPosition += _DefaultFontSize + _FontHeightMargin;
++            } else {
++                this.scrollUp();
++            }
             // TODO: Handle scrolling. (Project 1)
             //size of buffer is 29
         };
+        
++        //Used to scroll screen
++        Console.prototype.scrollUp = function () {
++            var yOffset = _DefaultFontSize + _FontHeightMargin;
++            var image = _DrawingContext.getImageData(0, yOffset, _DrawingContext.canvas.width, _DrawingContext.canvas.height);
++            _DrawingContext.putImageData(image, 0, 0);
++            _DrawingContext.clearRect(0, _DrawingContext.canvas.height - yOffset, _DrawingContext.canvas.width, _DrawingContext.canvas.height);
++        };
+
         Console.prototype.backSpace = function (text) {
-            var length = _DrawingContext.measureText(this.currentFont, this.currentFontSize, text);
+            var textLength = _DrawingContext.measureText(this.currentFont, this.currentFontSize, text);
             var height = _DefaultFontSize + _FontHeightMargin;
-            _DrawingContext.clearRect(this.currentXPosition - length, ((this.currentYPosition - height) + 5), length, height);
+            _DrawingContext.clearRect(this.currentXPosition - textLength, ((this.currentYPosition - height) + 3), textLength, height + 3);
             if (this.currentXPosition > 0) {
-                this.currentXPosition = this.currentXPosition - length;
+                this.currentXPosition = this.currentXPosition - textLength;
             }
+            else {
++                this.currentXPosition = 0;
++
++                //More input?
++                if (this.buffer.length > 0) {
++                    this.currentYPosition = this.currentYPosition - (_DefaultFontSize + _FontHeightMargin);
++                    var testLength = _DrawingContext.measureText(this.buffer);
++                    alert(testLength);
++                    this.currentXPosition = testLength % 500;
++                    this.backSpace(text);
++                }
+             }
         };
+        
         //Checks for smaller string in larger string
         Console.prototype.contains = function (smallString, largeString) {
             var stillMatching = true;
