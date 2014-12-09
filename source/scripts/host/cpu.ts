51662c84ebf59f5405fb10fd7f7022001b205a2d
RobertDigilio2 <elitemajor02@hotmail.com>
2014-12-09 13:29:53 -0500

    Update 1.75
    Created memory and made visible, can run stored program in memory, and can step through program execution.

--- source/scripts/host/cpu.ts
+++ source/scripts/host/cpu.ts
@@ -17,18 +17,21 @@
 
 module TSOS {
 
-    export class Cpu {
+    export class Cpu 
+    {
 
         constructor(public PC: number = 0,
                     public Acc: number = 0,
                     public Xreg: number = 0,
                     public Yreg: number = 0,
                     public Zflag: number = 0,
-                    public isExecuting: boolean = false) {
+                    public isExecuting: boolean = false) 
+        {
 
         }
 
-        public init(): void {
+        public init(): void 
+        {
             this.PC = 0;
             this.Acc = 0;
             this.Xreg = 0;
@@ -37,10 +40,206 @@ module TSOS {
             this.isExecuting = false;
         }
 
-        public cycle(): void {
+        public cycle(): void 
+        {
             _Kernel.krnTrace('CPU cycle');
-            // TODO: Accumulate CPU usage and profiling statistics here.
-            // Do the real work here. Be sure to set this.isExecuting appropriately.
+            this.handleCommand(_MemoryHandler.read(this.PC));
+        }
+
+        public updateConsole()
+        {
+                _MemoryElement.value += "\n \n";
+                _MemoryElement.value += "PC: " + this.PC + "\n";
+                _MemoryElement.value += "Acc: " + this.Acc + "\n";
+                _MemoryElement.value += "Xreg: " + this.Xreg + "\n";
+                _MemoryElement.value += "Yreg: " + this.Yreg + "\n";
+                _MemoryElement.value += "Zflag: " + this.Zflag + "\n";
+        }
+        public load(PC, Acc, Xreg, Yreg, Zflag)
+        {
+            this.PC = PC;
+            this.Acc = Acc;
+            this.Xreg = Yreg;
+            this.Zflag = Zflag;
+        }
+
+        public handleCommand(command)
+        {
+            switch (command) {
+                case "AC":
+                {
+                    //Load y register from memory
+                    var oldPC = this.PC;
+                    this.PC = parseInt("0x" + _MemoryHandler.read(this.PC + 2) + _MemoryHandler.read(this.PC + 1));
+                    this.Yreg = parseInt("0x" + _MemoryHandler.read(this.PC));
+                    this.PC = oldPC + 3;
+                    _MemoryHandler.updateMem();
+                    break;
+                }
+                case "AD":
+                {
+                    //Load from memory
+                    var oldPC = this.PC;
+                    this.PC = parseInt("0x" + _MemoryHandler.read(this.PC + 2) + _MemoryHandler.read(this.PC + 1));
+                    this.Acc = parseInt("0x" + _MemoryHandler.read(this.PC));
+                    this.PC = oldPC + 3;
+                    _MemoryHandler.updateMem();
+                    break;
+                }
+                case "AE":
+                {
+                    //Load x register from memory
+                    var oldPC = this.PC;
+                    this.PC = parseInt("0x" + _MemoryHandler.read(this.PC + 2) + _MemoryHandler.read(this.PC + 1));
+                    this.Xreg = parseInt("0x" + _Memory[this.PC]);
+                    this.PC = oldPC + 3;
+                    _MemoryHandler.updateMem();
+                    break;
+                }
+                case "A0":
+                {
+                    //Load constant into y register
+                    this.Yreg = parseInt("0x" + (_MemoryHandler.read(this.PC + 1)));
+                    this.PC = this.PC + 2;
+                    _MemoryHandler.updateMem();
+                    break;
+
+                }
+                case "A2":
+                {
+                  //Load constant into x register
+                    this.Xreg = parseInt("0x" + (_MemoryHandler.read(this.PC + 1)));
+                    this.PC = this.PC + 2;
+                    _MemoryHandler.updateMem();
+                    break;
+                }
+                case "A9":
+                {
+                    //Load a constant
+                    this.Acc = parseInt("0x" + (_MemoryHandler.read(this.PC + 1)));
+                    this.PC = this.PC + 2;
+                    _MemoryHandler.updateMem();
+                    break;
+                }
+                case "D0":
+                {
+                    //BEQ if z flag is not set, branch
+                    if(this.Zflag == 0)
+                    {
+                        this.PC =this.PC - (255 - parseInt("0x" + _MemoryHandler.read(this.PC + 1))) + 1;
+                    }
+                    else
+                    {
+                        this.PC = this.PC + 2;
+                    }
+                    _MemoryHandler.updateMem();
+                    break;
+                }
+                case "EA":
+                {
+                    //Nothing op code
+                    this.PC = this.PC + 1;
+                    break;
+                }
+                case "EC":
+                {
+                    //Equals compare of memory to the Xreg
+                    //First get memory variable
+                    var oldPC = this.PC;
+                    this.PC = parseInt("0x" + _MemoryHandler.read(this.PC + 2) + _MemoryHandler.read(this.PC + 1));
+
+                    var temp = parseInt("0x" + _MemoryHandler.read(this.PC));
+                    if(temp == this.Xreg)
+                    {
+                        this.Zflag = 1;
+                    }
+                    this.PC = oldPC + 3;
+                    _MemoryHandler.updateMem();
+                    break;
+                }
+                case "EE":
+                {
+                    //Increment the byte
+                    //First read it
+                    var oldPC = this.PC;
+                    this.PC = parseInt("0x" + _MemoryHandler.read(this.PC + 2) + _MemoryHandler.read(this.PC + 1));
+                    var temp = parseInt("0x" + _MemoryHandler.read(this.PC));
+                    temp = temp + 1;
+                    _MemoryHandler.load(temp, this.PC);
+                    _MemoryHandler.updateMem();
+                    break;
+
+                }
+                case "FF":
+                {
+                    //System Call, check the Xreg
+                    if(this.Xreg == 2)
+                    {
+                        _StdOut.advanceLine();
+                       //print the yreg to the screen
+                        var i = 0;
+                        while(_MemoryHandler.read(this.Yreg + i) != "00" && i < 256)
+                        {
+                            var charCode = (parseInt("0x" +_MemoryHandler.read(this.Yreg + i).toString()));
+                            var char = String.fromCharCode(charCode);
+                            _StdOut.putText(char);
+                            i++;
+                        }
+                    }
+                    if(this.Xreg == 1)
+                    {
+                        _StdOut.advanceLine();
+                        _StdOut.putText(" " + this.Yreg);
+                    }
+                    _MemoryHandler.updateMem();
+                    this.PC += 1;
+                    break;
+                }
+                case "00":
+                {
+                    //Break
+                   this.isExecuting = false;
+                    _MemoryHandler.updateMem();
+                    document.getElementById("btnStep").disabled = true;
+                    break;
+                }
+                case "6D":
+                {
+                    //Add with carry
+                    var oldPC = this.PC;
+                    this.PC = parseInt("0x" + _MemoryHandler.read(this.PC + 2) + _MemoryHandler.read(this.PC + 1));
+                    this.Acc += parseInt("0x" + _MemoryHandler.read(this.PC));
+                    this.PC = oldPC + 3;
+                    _MemoryHandler.updateMem();
+                    break;
+                }
+                case "8D":
+                {
+                    //Store to memory
+                    var memLoc = parseInt("0x" + _MemoryHandler.read(this.PC + 2) + _MemoryHandler.read(this.PC + 1));
+                    if(this.Acc < 16)
+                    {
+                        _MemoryHandler.load(("0" + this.Acc), memLoc);
+                    }
+                    else
+                    {
+                        _MemoryHandler.load(this.Acc, memLoc);
+                    }
+                    this.PC += 3;
+                    _MemoryHandler.updateMem();
+                    break;
+                }
+                default:
+                {
+                    //Not a valid hexcode
+                    _DrawingContext.putText("Invalid Code");
+                    _DrawingContext.advanceLine();
+                    this.isExecuting = false;
+                    _MemoryHandler.updateMem();
+                    break;
+
+                }
+             }
         }
     }
 }
