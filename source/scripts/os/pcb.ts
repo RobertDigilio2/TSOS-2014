c2d0ecea0a205ca9dbe85829d3acdf3d80fb892a
RobertDigilio2 <elitemajor02@hotmail.com>
2014-12-10 13:27:15 -0500

    Update 2.0
    Displays PCB on screen, saves state of the CPU back to the PCB, and fixes some minor bugs. Can also see PC contents and memory.

--- source/scripts/os/pcb.ts
+++ source/scripts/os/pcb.ts
@@ -8,16 +8,37 @@ module TSOS{
 
         public PCB( PC: number = 0, Acc: number = 0, Xreg: number = 0, Yreg: number = 0, Zflag: number = 0)
         {
-            this.PC = 0;
-            this.Acc = 0;
-            this.Xreg = 0;
-            this.Yreg = 0;
-            this.Zflag = 0;
+            this.PC = PC;
+            this.Acc = Acc;
+            this.Xreg = Xreg;
+            this.Yreg = Yreg;
+            this.Zflag = Zflag;
         }
+        
+        //Loads values of PCB to CPU
         public loadToCPU()
         {
             _CPU.load(this.PC, this.Acc, this.Xreg, this.Yreg, this.Zflag);
-            _CPU.isExecuting = true;
+        }
+        
+        //Method to store the parameters to this PCB
+        public storeVals(PC, Acc, Xreg, Yreg, Zflag)
+        {
+            this.PC= PC;
+            this.Acc = Acc;
+            this.Xreg = Xreg;
+            this.Yreg = Yreg;
+            this.Zflag = Zflag;
+        }
+
+        public printToScreen()
+        {
+            _MemoryElement.value += "\n";
+            _MemoryElement.value += "PC: " + this.PC + "\n";
+            _MemoryElement.value += "Acc: " + this.Acc + "\n";
+            _MemoryElement.value += "Xreg: " + this.Xreg + "\n";
+            _MemoryElement.value += "Yreg: " + this.Yreg + "\n";
+            _MemoryElement.value += "Zflag: " + this.Zflag + "\n";
         }
     }
 }
