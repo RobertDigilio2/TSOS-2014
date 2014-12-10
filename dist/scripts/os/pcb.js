dd2bbb400c072ae12b22208f689da270b0facd63
RobertDigilio2 <elitemajor02@hotmail.com>
2014-12-10 13:27:17 -0500

    Update 2.0
    Displays PCB on screen, saves state of the CPU back to the PCB, and fixes some minor bugs. Can also see PC contents and memory.

--- dist/scripts/os/pcb.js
+++ dist/scripts/os/pcb.js
@@ -16,16 +16,34 @@ var TSOS;
             if (typeof Xreg === "undefined") { Xreg = 0; }
             if (typeof Yreg === "undefined") { Yreg = 0; }
             if (typeof Zflag === "undefined") { Zflag = 0; }
-            this.PC = 0;
-            this.Acc = 0;
-            this.Xreg = 0;
-            this.Yreg = 0;
-            this.Zflag = 0;
+            this.PC = PC;
+            this.Acc = Acc;
+            this.Xreg = Xreg;
+            this.Yreg = Yreg;
+            this.Zflag = Zflag;
         };
-
+        
+        //Loads values of PCB to CPU
         PCB.prototype.loadToCPU = function () {
             _CPU.load(this.PC, this.Acc, this.Xreg, this.Yreg, this.Zflag);
-            _CPU.isExecuting = true;
+        };
+
+        //Method to store the parameters to this PCB
+        PCB.prototype.storeVals = function (PC, Acc, Xreg, Yreg, Zflag) {
+            this.PC = PC;
+            this.Acc = Acc;
+            this.Xreg = Xreg;
+            this.Yreg = Yreg;
+            this.Zflag = Zflag;
+        };
+
+        PCB.prototype.printToScreen = function () {
+            _MemoryElement.value += "\n";
+            _MemoryElement.value += "PC: " + this.PC + "\n";
+            _MemoryElement.value += "Acc: " + this.Acc + "\n";
+            _MemoryElement.value += "Xreg: " + this.Xreg + "\n";
+            _MemoryElement.value += "Yreg: " + this.Yreg + "\n";
+            _MemoryElement.value += "Zflag: " + this.Zflag + "\n";
         };
         return PCB;
     })();
