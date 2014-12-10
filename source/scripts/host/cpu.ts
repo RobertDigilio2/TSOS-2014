4d8426b87f15d5f413a2749bb34f65b4edd19789
RobertDigilio2 <elitemajor02@hotmail.com>
2014-12-10 13:26:58 -0500

    Update 2.0
    Displays PCB on screen, saves state of the CPU back to the PCB, and fixes some minor bugs. Can also see PC contents and memory.

--- source/scripts/host/cpu.ts
+++ source/scripts/host/cpu.ts
@@ -46,23 +46,34 @@ module TSOS {
             this.handleCommand(_MemoryHandler.read(this.PC));
         }
 
-        public updateConsole()
+        //Updates UI
+        public updateUI()
         {
-                _MemoryElement.value += "\n \n";
+                _MemoryElement.value += "\n \n CPU \n";
                 _MemoryElement.value += "PC: " + this.PC + "\n";
                 _MemoryElement.value += "Acc: " + this.Acc + "\n";
                 _MemoryElement.value += "Xreg: " + this.Xreg + "\n";
                 _MemoryElement.value += "Yreg: " + this.Yreg + "\n";
                 _MemoryElement.value += "Zflag: " + this.Zflag + "\n";
         }
+        
+        //Loads CPU with specific values
         public load(PC, Acc, Xreg, Yreg, Zflag)
         {
             this.PC = PC;
             this.Acc = Acc;
-            this.Xreg = Yreg;
+            this.Xreg = Xreg;
+            this.Yreg = Yreg;
             this.Zflag = Zflag;
         }
 
+        //Stores current CPU values to PID
+        public storeInPCB(PID)
+        {
+            _Processes[PID - 1].storeVals(this.PC, this.Acc, this.Xreg, this.Yreg, this.Zflag);
+        }
+
+        //Handles disassembly command
         public handleCommand(command)
         {
             switch (command) {
@@ -126,7 +137,17 @@ module TSOS {
                     //BEQ if z flag is not set, branch
                     if(this.Zflag == 0)
                     {
-                        this.PC =this.PC - (255 - parseInt("0x" + _MemoryHandler.read(this.PC + 1))) + 1;
+                        var offset = parseInt("0x" + _MemoryHandler.read(this.PC + 1));
+                        this.PC = this.PC + offset;
+                        if (this.PC > 255)
+                        {
+                            this.PC = this.PC - 255;
+                        }
+                        else
+                        {
+                            this.PC = this.PC + 1;
+                        }
+                        this.PC = this.PC + 1;
                     }
                     else
                     {
@@ -147,11 +168,15 @@ module TSOS {
                     //First get memory variable
                     var oldPC = this.PC;
                     this.PC = parseInt("0x" + _MemoryHandler.read(this.PC + 2) + _MemoryHandler.read(this.PC + 1));
-
                     var temp = parseInt("0x" + _MemoryHandler.read(this.PC));
+                    
                     if(temp == this.Xreg)
                     {
                         this.Zflag = 1;
+                    else
+                    {
+                        this.Zflag = 0;
+                    }
                     }
                     this.PC = oldPC + 3;
                     _MemoryHandler.updateMem();
@@ -175,7 +200,6 @@ module TSOS {
                     //System Call, check the Xreg
                     if(this.Xreg == 2)
                     {
-                        _StdOut.advanceLine();
                        //print the yreg to the screen
                         var i = 0;
                         while(_MemoryHandler.read(this.Yreg + i) != "00" && i < 256)
@@ -188,8 +212,7 @@ module TSOS {
                     }
                     if(this.Xreg == 1)
                     {
-                        _StdOut.advanceLine();
-                        _StdOut.putText(" " + this.Yreg);
+                        _StdOut.putText("" + this.Yreg);
                     }
                     _MemoryHandler.updateMem();
                     this.PC += 1;
@@ -199,8 +222,10 @@ module TSOS {
                 {
                     //Break
                    this.isExecuting = false;
+                    _CPU.storeInPCB(_currentProcess);
                     _MemoryHandler.updateMem();
                     document.getElementById("btnStep").disabled = true;
+                    _currentProcess = 0;
                     break;
                 }
                 case "6D":
