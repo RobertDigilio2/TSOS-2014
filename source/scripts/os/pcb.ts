58731e9654e291f27f4ba20469c65475bcf9fa40
RobertDigilio2 <elitemajor02@hotmail.com>
2014-12-11 13:04:20 -0500

    Update 2.25
    Can now load and run 3 programs.

--- source/scripts/os/pcb.ts
+++ source/scripts/os/pcb.ts
@@ -5,6 +5,7 @@ module TSOS{
         public Xreg: number = 0;
         public Yreg: number = 0;
         public Zflag: number = 0;
++        public PID: number = 0;
 
         public PCB( PC: number = 0, Acc: number = 0, Xreg: number = 0, Yreg: number = 0, Zflag: number = 0)
         {
@@ -14,7 +15,7 @@ module TSOS{
             this.Yreg = Yreg;
             this.Zflag = Zflag;
         }
-        
+
         //Loads values of PCB to CPU
         public loadToCPU()
         {
@@ -30,15 +31,26 @@ module TSOS{
             this.Yreg = Yreg;
             this.Zflag = Zflag;
         }
+        
+        public setPID(val)
+        {
+            this.PID = val;
+        }
+
+        public setPCval(val)
+        {
+            this.PC = val;
+        }
 
         public printToScreen()
         {
             _MemoryElement.value += "\n";
-            _MemoryElement.value += "PC: " + this.PC + "\n";
-            _MemoryElement.value += "Acc: " + this.Acc + "\n";
-            _MemoryElement.value += "Xreg: " + this.Xreg + "\n";
-            _MemoryElement.value += "Yreg: " + this.Yreg + "\n";
-            _MemoryElement.value += "Zflag: " + this.Zflag + "\n";
+            _MemoryElement.value += "PC: " + this.PC + "|";
+            _MemoryElement.value += "Acc: " + this.Acc + "|";
+            _MemoryElement.value += "Xreg: " + this.Xreg + "|";
+            _MemoryElement.value += "Yreg: " + this.Yreg + "|";
+            _MemoryElement.value += "Zflag: " + this.Zflag + "|";
+            _MemoryElement.value += "PCBID: " + this.PID;
         }
     }
 }
